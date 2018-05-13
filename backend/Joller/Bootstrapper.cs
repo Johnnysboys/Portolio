07a03233db5d1c976b192272c9147f78401173b2
Simon Sinding <sinding2000@gmail.com>
2018-05-14 01:48:22 +0200

    added authentication to bootstrapper

--- backend/Joller/Bootstrapper.cs
+++ backend/Joller/Bootstrapper.cs
@@ -1,22 +1,38 @@
+using System;
 using Nancy;
 using Nancy.Bootstrapper;
 using Nancy.TinyIoc;
-
-using Joller.Models;
+using Joller.Authentication;
+using Joller.Repositories;
 using Joller.Repositories.Interfaces;
+using Nancy.Authentication.Stateless;
 
 namespace Joller
 {
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+
+
         private readonly ISubscriberRepository _subscriberRepository;
+        private readonly IUserRepository _userRepository;
         public Bootstrapper()
         {
-
         }
-        public Bootstrapper(ISubscriberRepository subscriberRepository)
+        public Bootstrapper(string mongodbUrl)
         {
-            this._subscriberRepository = subscriberRepository;
+
+            this._subscriberRepository = new SubscriberRepository(mongodbUrl);
+            this._userRepository = new UserRepository(mongodbUrl);
+        }
+
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            var StatelessAuthConfigurationFactory = container.Resolve<IStatelessAuthConfigurationFactory>();
+            var configuration = StatelessAuthConfigurationFactory.Create();
+
+            StatelessAuthentication.Enable(pipelines, configuration);
         }
 
         protected override void ConfigureApplicationContainer(TinyIoCContainer container)
@@ -24,6 +40,7 @@ namespace Joller
             base.ConfigureApplicationContainer(container);
 
             container.Register(_subscriberRepository);
+            container.Register(_userRepository);
         }
     }
 }
