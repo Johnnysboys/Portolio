315428a5ba021e473602d13523a6fd8457df3379
Simon Sinding <sinding2000@gmail.com>
2018-05-15 00:36:14 +0200

    added id to "post"

--- backend/Joller/Models/AboutMe.cs
+++ backend/Joller/Models/AboutMe.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Joller.Models
 {
     public class AboutMe
     {
+        [BsonId]
+        public ObjectId internalId;
         public string Text;
     }
 }
