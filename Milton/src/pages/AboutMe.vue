938018a094d083eeaaa37a721bf8a99d65392b31
Simon Sinding <sinding2000@gmail.com>
2018-05-15 00:33:43 +0200

    About me page is done

---
+++ Milton/src/pages/AboutMe.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page padding>
+    <h5>About me</h5>
+    <div class="row justify-center">
+      <span v-html="post" />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+export default {
+  name: 'aboutme',
+  created() {
+    if (this.post === '') {
+      this.fetchNewestPost();
+    }
+  },
+  data() {
+    return {
+      text: 'Fucker'
+    };
+  },
+  computed: {
+    ...mapGetters('posts', ['post'])
+  },
+  methods: {
+    ...mapActions('posts', ['fetchNewestPost'])
+  }
+};
+</script>
