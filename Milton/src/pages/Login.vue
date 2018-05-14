b2d8acbbed38a602c0da01b0fc50dfe85cfe0375
Simon Sinding <sinding2000@gmail.com>
2018-05-15 00:34:02 +0200

    login works

---
+++ Milton/src/pages/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-page padding >
+    <h3>Login to Milton</h3>
+    <q-field icon="email">
+      <q-input v-model="email" float-label="Email"/>
+    </q-field>
+    <q-field icon="lock">
+      <q-input v-model="password" type="password" float-label="Password"/>
+    </q-field>
+    <q-btn color="primary" @click="loginHandler">Login</q-btn>
+  </q-page>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    };
+  },
+  methods: {
+    ...mapActions('auth', ['login']),
+    loginHandler() {
+      this.login({ email: this.email, password: this.password })
+        .then(_ => {
+          this.$router.push('/');
+        })
+        .catch(err => {
+          console.log(err.message);
+        });
+    }
+  }
+};
+</script>
