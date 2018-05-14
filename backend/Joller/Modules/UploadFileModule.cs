880c0e9090a186686955870e10d63408dad1f883
Simon Sinding <sinding2000@gmail.com>
2018-05-15 00:38:07 +0200

    added file lister

--- backend/Joller/Modules/UploadFileModule.cs
+++ backend/Joller/Modules/UploadFileModule.cs
@@ -8,19 +8,30 @@ namespace Joller.Modules
     public class UploadFileModule : NancyModule
     {
         private readonly IFileUploadHandler fileUploadHandler;
+        private readonly ILocalFileServer fileServer;
 
-        public UploadFileModule(IFileUploadHandler fileUploadHandler)
+        public UploadFileModule(IFileUploadHandler fileUploadHandler, ILocalFileServer LocalFileServer)
             : base("/file")
         {
 
             // It requires you to be an admin to upload
-            this.RequiresAuthentication();
-            this.RequiresClaims(c => c.Value.Equals("admin"));
 
             this.fileUploadHandler = fileUploadHandler;
-
+            this.fileServer = LocalFileServer;
+            Get("/", async args =>
+            {
+                var list = await this.fileServer.ListFiles();
+                var res = new
+                {
+                    Status = "success",
+                    Data = list
+                };
+                return Response.AsJson(res);
+            });
             Post("/upload", async parameters =>
             {
+                this.RequiresAuthentication();
+                this.RequiresClaims(c => c.Value.Equals("admin"));
                 var request = this.Bind<FileUploadRequest>();
 
                 var uploadResult = await fileUploadHandler.HandleUpload(request.File.Name, request.File.Value);
