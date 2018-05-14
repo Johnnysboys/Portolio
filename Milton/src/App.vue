b91d05cd84464fcf6b930d995e4ca8c8df1285a4
Simon Sinding <sinding2000@gmail.com>
2018-05-15 00:33:16 +0200

    frontend init

---
+++ Milton/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  beforeCreate() {
+    // Check if the jwt key is stored
+    this.$store.dispatch('auth/checkStorage');
+  }
+};
+</script>
+
+<style>
+
+</style>
